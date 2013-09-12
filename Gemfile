b9b427f14214f227ead5024dfd61227e3cf2230a
Mukund Lakshman <yaymukund@gmail.com>
2013-09-12 10:10:14 -0400

    Initial commit with everything

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'haml'
+gem 'kaminari'
+
+gem 'ember-rails', github: 'emberjs/ember-rails'
+gem 'ember-source'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+
+# Use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :development do
+  gem 'pry-rails'
+  gem 'guard'
+  gem 'guard-rspec'
+end
+
+group :test, :development do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+  gem 'fabrication'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hellopoem::Application.config.secret_key_base = 'd474548797a08582c59c115302ddd0c50e25165fc49d0372c02f47fcdf2c69bf2d10841901c6dcd388003db5450d410c4ffbeaa90a75d1581a1e547242102eca'
